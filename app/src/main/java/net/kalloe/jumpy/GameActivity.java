66ee87833ee4fb999db532adaaca630cb0e439d3
JamieKalloe <Jamie@Kalloe.net>
2016-02-24 20:18:10 +0100

    added game / physics engine (module) library

--- app/src/main/java/net/kalloe/jumpy/GameActivity.java
+++ app/src/main/java/net/kalloe/jumpy/GameActivity.java
@@ -1,24 +1,11 @@
 package net.kalloe.jumpy;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class GameActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_game, menu);
-        return true;
-    }
+    
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
