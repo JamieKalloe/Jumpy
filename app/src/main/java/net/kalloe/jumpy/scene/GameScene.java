845767ff15e4ac409f439bc5efdd2395f0edb645
JamieKalloe <Jamie@Kalloe.net>
2016-02-28 21:36:59 +0100

    configred accelerometer functionality to the engine and added collision detection of sprites

--- app/src/main/java/net/kalloe/jumpy/scene/GameScene.java
+++ app/src/main/java/net/kalloe/jumpy/scene/GameScene.java
@@ -6,21 +6,33 @@ import net.kalloe.jumpy.entity.Player;
 import net.kalloe.jumpy.factory.PlayerFactory;
 
 import org.andengine.engine.camera.hud.HUD;
+import org.andengine.engine.handler.collision.CollisionHandler;
+import org.andengine.engine.handler.collision.ICollisionCallback;
 import org.andengine.entity.Entity;
+import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.entity.scene.IOnSceneTouchListener;
+import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.EntityBackground;
+import org.andengine.entity.shape.IShape;
+import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.entity.text.Text;
 import org.andengine.entity.text.TextOptions;
+import org.andengine.input.sensor.acceleration.AccelerationData;
+import org.andengine.input.sensor.acceleration.IAccelerationListener;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.util.adt.align.HorizontalAlign;
+import org.andengine.util.adt.color.Color;
 
 /**
  * Created by Jamie on 28-2-2016.
  */
-public class GameScene extends AbstractScene {
+public class GameScene extends AbstractScene implements IAccelerationListener {
 
     //Variables
     private Player player;
     private Text scoreText;
+    private AnimatedSprite fly;
 
     public GameScene() {
         //Creates a new instance of the Player
@@ -32,22 +44,82 @@ public class GameScene extends AbstractScene {
      */
     @Override
     public void populate() {
+        //Create and load the game entities.
         createBackground();
         createPlayer();
+        createEnemy();
         createHUD();
 
-        //Test toast
-//        this.showToast(activity.getString(R.string.hello_world), Toast.LENGTH_LONG);
+        //Enables touch events for the player object (sprite).
+        registerTouchArea(player);
+
+        //Enables the accelerometer and registers the listener to the engine.
+        engine.enableAccelerationSensor(activity, this);
+
+        setOnSceneTouchListener(new IOnSceneTouchListener() {
+
+            @Override
+            public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+                //Checks whether the TouchEvent is an ACTION_DOWN event (tap on the screen).
+                if (pSceneTouchEvent.isActionDown()) {
+                    player.clearEntityModifiers();
+                    player.registerEntityModifier(new MoveModifier(1, player.getX(), player.getY(),
+                            pSceneTouchEvent.getX(), pSceneTouchEvent.getY()));
+                    return true;
+                }
+                return false;
+            }
+
+        });
+
+        //Collision detection of the player and enemies.
+        ICollisionCallback playerCollision = new ICollisionCallback() {
+
+            @Override
+            public boolean onCollision(IShape pCheckShape, IShape pTargetShape) {
+                fly.setColor(Color.RED);
+                return false;
+            }
+
+        };
+
+        //Binds the player and enemy objects to check for collision of the sprites.
+        CollisionHandler collisionHandler = new CollisionHandler(playerCollision, fly, player);
+        registerUpdateHandler(collisionHandler);
+    }
+
+    @Override
+    public void onAccelerationAccuracyChanged(AccelerationData pAccelerationData) {
+
+    }
+
+    float lastX = 0;
+    @Override
+    public void onAccelerationChanged(final AccelerationData pAccelerationData) {
+        //The following code is responsible for calculating the direction the player is facing.
+        //According to the result, the player will turn left or right (the sprite will be flipped).
+        if(Math.abs(pAccelerationData.getX() - lastX) > 0.5) {
+            if(pAccelerationData.getX() > 0) {
+                player.turnRight();
+            } else {
+                player.turnLeft();
+            }
+            lastX = pAccelerationData.getX();
+        }
+
+        player.setX(player.getX() + pAccelerationData.getX());
     }
 
     @Override
     public void onPause() {
-
+        //Disables the accelerometer sensor when the activity is not active.
+        engine.disableAccelerationSensor(activity);
     }
 
     @Override
     public void onResume() {
-
+        //Enables the accelerometer sensor when the activity is being resumed.
+        engine.enableAccelerationSensor(activity, this);
     }
 
     /**
@@ -110,5 +182,25 @@ public class GameScene extends AbstractScene {
                 Toast.makeText(activity, text, length).show();
             }
         });
+    }
+
+
+    public void createEnemy() {
+        fly = new AnimatedSprite(240, 200, res.enemyTextureRegion, vbom) {
+
+            @Override
+            protected void onManagedUpdate(float pSecondsElapsed) {
+                super.onManagedUpdate(pSecondsElapsed);
+
+                if(collidesWith(player)) {
+                    setScale(2);
+                } else {
+                    setScale(1);
+                }
+            }
+        };
+
+        fly.animate(125);
+        attachChild(fly);
     }
 }
