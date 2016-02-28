e935faf9848e5f3ad5d03599f770b695756e0047
JamieKalloe <Jamie@Kalloe.net>
2016-02-28 15:37:36 +0100

    added game HUD to the Game Scene

--- app/src/main/java/net/kalloe/jumpy/scene/GameScene.java
+++ app/src/main/java/net/kalloe/jumpy/scene/GameScene.java
@@ -1,11 +1,17 @@
 package net.kalloe.jumpy.scene;
 
+import android.widget.Toast;
+
 import net.kalloe.jumpy.entity.Player;
 import net.kalloe.jumpy.factory.PlayerFactory;
 
+import org.andengine.engine.camera.hud.HUD;
 import org.andengine.entity.Entity;
 import org.andengine.entity.scene.background.EntityBackground;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.Text;
+import org.andengine.entity.text.TextOptions;
+import org.andengine.util.adt.align.HorizontalAlign;
 
 /**
  * Created by Jamie on 28-2-2016.
@@ -14,6 +20,7 @@ public class GameScene extends AbstractScene {
 
     //Variables
     private Player player;
+    private Text scoreText;
 
     public GameScene() {
         //Creates a new instance of the Player
@@ -27,6 +34,10 @@ public class GameScene extends AbstractScene {
     public void populate() {
         createBackground();
         createPlayer();
+        createHUD();
+
+        //Test toast
+//        this.showToast(activity.getString(R.string.hello_world), Toast.LENGTH_LONG);
     }
 
     @Override
@@ -65,5 +76,39 @@ public class GameScene extends AbstractScene {
 
         //Attaches the Player entity / object to the Scene entity.
         attachChild(player);
+    }
+
+    /**
+     * Creates a new heads-up display and attaches it to the camera (has a special camera-scene).
+     */
+    private void createHUD() {
+        //Creates a new heads-up display (HUD)
+        HUD hud = new HUD();
+
+        //Creates a new text label using the font from the ResourceManager.
+        scoreText = new Text(16, 789, res.font, "0123456789", new TextOptions(HorizontalAlign.LEFT), vbom);
+        scoreText.setAnchorCenter(0, 1);
+
+        //Attaches the text object to the hud.
+        hud.attachChild(scoreText);
+
+        //A HUD needs to be attached to the camera, because it uses a special camera-scene.
+        //If the HUD is attached to the game scene, the text would leave the screen when the player moves.
+        camera.setHUD(hud);
+    }
+
+    /**
+     * Creates and shows a Toast message using the Android Toast widget.
+     * A Toast is run on te UI Thread which every activity has.
+     * @param text Text to be displayed by the Toast.
+     * @param length Length of the Toast to be displayed.
+     */
+    public void showToast(final String text, final int length) {
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(activity, text, length).show();
+            }
+        });
     }
 }
