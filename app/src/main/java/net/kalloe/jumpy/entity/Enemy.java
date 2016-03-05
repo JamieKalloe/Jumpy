09a9bd9da3ff68fac81df4bc4fc83c22ba6605b7
JamieKalloe <Jamie@Kalloe.net>
2016-03-05 20:48:30 +0100

    created the player (entity) class

---
+++ app/src/main/java/net/kalloe/jumpy/entity/Enemy.java
@@ -0,0 +1,56 @@
+package net.kalloe.jumpy.entity;
+
+import com.badlogic.gdx.physics.box2d.Body;
+
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+
+/**
+ * Created by Jamie on 5-3-2016.
+ */
+public class Enemy extends AnimatedSprite implements CollidableEntity {
+
+    //Variables
+    public static final String TYPE = "ENEMY";
+    private Body body;
+
+    /**
+     * Creates a new instance of the enemy entity.
+     * @param pX coordinates / postion of the enemy animated sprite.
+     * @param pY coordinates / postion of the enemy animated sprite.
+     * @param pITiledTextureRegion enemy animated sprite / texture
+     * @param pVertexBufferObjectManager vertex buffer object manager
+     */
+    public Enemy(float pX, float pY, ITiledTextureRegion pITiledTextureRegion, VertexBufferObjectManager pVertexBufferObjectManager) {
+        super(pX, pY, pITiledTextureRegion, pVertexBufferObjectManager);
+    }
+
+
+    /**
+     * Sets the physics body of the Enemy entity.
+     * @param body physics body of the Enemy.
+     */
+    @Override
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    /**
+     * Returns the physics body of the Enemy entity.
+     * @return physics body of the Enemy.
+     */
+    @Override
+    public Body getBody() {
+        return this.body;
+    }
+
+    /**
+     * Returns the String type of the Enemy entity.
+     * @return String type
+     */
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+}
